6babc0f238870c32261441381fcc29d44030214b
Carlos Henrique Nascimento <chnluiz@id.uff.br>
2026-06-10 00:03:46 -0300

    Fix formatting in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const REPO_NAME = '/Dev-Web-Trabalho-Pratico-Parte-2'
+const REPO_NAME = '/Dev-Web-Trabalho-Pratico-Parte-2/'
 
 export default defineConfig({
   plugins: [react()],
