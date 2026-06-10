c4c6477d76eba74b8c3ffaa2d8f389e830d78254
Iucio <75343485+Iucio@users.noreply.github.com>
2026-06-09 23:04:38 -0300

    Update vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const REPO_NAME = '/Dev-Web-Trabalho-Pratico-Parte-2'
+
+export default defineConfig({
+  plugins: [react()],
+  
+  base: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
+})
